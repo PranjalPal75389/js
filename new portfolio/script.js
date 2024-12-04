ed266d97bfbf4a762366555694ffc9c9ce255d52
Pranjal Pal <palpranjal75389@gmail.com>
2024-12-04 12:06:27 +0530

    files updated

---
+++ new portfolio/script.js
@@ -0,0 +1,9 @@
+const globalConfig = {
+    "strokeBottom": 5,
+  
+    "round": true,
+    /* e.t.c */
+  }
+  
+  const global = new CircularProgressBar('global', globalConfig);
+  global.initial();
