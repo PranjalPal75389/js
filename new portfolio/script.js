4fddb1566afd977e56ce63ab076211cf6c0a84e6
Pranjal Pal <palpranjal75389@gmail.com>
2024-12-05 21:53:35 +0530

    form sumubmition & animation added

--- new portfolio/script.js
+++ new portfolio/script.js
@@ -1,3 +1,13 @@
+const menuBtn=document.getElementsByClassName("menu-bar")[0];
+const navBar=document.getElementsByClassName("navbar")[0]
+
+function toggleMenu() {
+  navBar.classList.toggle("active");
+  console.log("clicked");
+}
+menuBtn.addEventListener("click", toggleMenu );
+
+
 const globalConfig = {
     "strokeBottom": 5,
   
@@ -7,3 +17,4 @@ const globalConfig = {
   
   const global = new CircularProgressBar('global', globalConfig);
   global.initial();
+
